e09b7587a0f95c0580475cfcb8ac4575d784a5a5
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-21 18:19:20 +0200

    add few constant and move camera

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,9 +1,16 @@
 import Grid from 'objects/Grid';
 
+const CameraVelocity = 10;
+const Bounds = 2000;
+const CellWidth = 50;
+
 class GameState extends Phaser.State {
 
 	create() {
-		this.gridLayout = new Grid(this.game, 9, 9, 50, 10, 10);
+		this.game.world.setBounds(0, 0, Bounds, Bounds);
+		this.gridLayout = new Grid(this.game, Bounds/ CellWidth, Bounds/CellWidth, CellWidth);
+		this.cursors = this.game.input.keyboard.createCursorKeys();
+
 	}
 
 	preload() {
@@ -11,7 +18,26 @@ class GameState extends Phaser.State {
 
 	update() {
 
-	}
+    if (this.cursors.up.isDown)
+    {
+      this.game.camera.y -= CameraVelocity;
+    }
+    else if (this.cursors.down.isDown)
+    {
+      this.game.camera.y += CameraVelocity;
+    }
+
+    if (this.cursors.left.isDown)
+    {
+    	this.game.camera.x -= CameraVelocity;
+    }
+    else if (this.cursors.right.isDown)
+    {
+    	this.game.camera.x += CameraVelocity;
+    }
+
+}
+
 
 }
 
