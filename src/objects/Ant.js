79e2bee14a7f1676e410aabdd3294b1a7fedd64f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-21 23:40:16 +0200

    wip move as langton algorithm

--- src/objects/Ant.js
+++ src/objects/Ant.js
@@ -1,14 +1,64 @@
 import { CellWidth } from '../constants';
 
+const Tilt = 90;
 class Ant extends Phaser.Sprite{
 
 	constructor(game, x, y) {
 		super(game, x, y, "ant", 0);
-		this.animations.add('down', [0, 1, 2], 10, true);
+		this.antRotation = 0;
+    this.animations.add('down', [0, 1, 2], 10, true);
     this.animations.add('left', [12, 13, 14], 10, true);
     this.animations.add('right', [24, 25, 25], 10, true);
     this.animations.add('up', [32, 33, 34], 10, true);
 	}
+
+  rotate() {
+    this.antRotation = (this.antRotation + Tilt) % 360;
+  }
+
+  updateAnt(cell) {
+    this.rotate();
+    if(cell.isChecked()) {
+      switch(this.antRotation) {
+        case 0:
+          this.goLeft();
+        break;
+        case 90:
+          this.goUp();
+        break;
+        case 180:
+          this.goUp();
+        break;
+        case 270:
+          this.goDown();
+        break;
+      }
+    }
+    else {
+      switch(this.antRotation) {
+        case 0:
+          this.goDown();
+        break;
+        case 90:
+          this.turnLeft();
+        break;
+        case 180:
+          this.turnRight();
+        break;
+        case 270:
+          this.goUp();
+        break;
+      }
+    }
+  }
+
+  goDown() {
+    this.y = this.y + CellWidth;
+  }
+
+  goUp() {
+    this.y = this.y - CellWidth;
+  }
 
   turnLeft() {
     this.x = this.x - CellWidth;
