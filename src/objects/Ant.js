616b55c4acec721c73d40050ff8962692f318cb8
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-21 19:55:16 +0200

    create animations

--- src/objects/Ant.js
+++ src/objects/Ant.js
@@ -1,8 +1,11 @@
 class Ant extends Phaser.Sprite{
 
 	constructor(game, x, y) {
-		super(game, x, y, "ant", 5);
-
+		super(game, x, y, "ant", 0);
+		this.animations.add('down', [0, 1, 2], 10, true);
+    this.animations.add('left', [12, 13, 14], 10, true);
+    this.animations.add('right', [24, 25, 25], 10, true);
+    this.animations.add('up', [32, 33, 34], 10, true);
 	}
 }
 
