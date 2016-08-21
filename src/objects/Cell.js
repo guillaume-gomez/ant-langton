b75119fac436655e6d1072c457f24afe48f87dd3
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-21 18:00:19 +0200

    add default color

--- src/objects/Cell.js
+++ src/objects/Cell.js
@@ -1,9 +1,9 @@
 class Cell extends Phaser.Graphics{
 
-	constructor(game, x, y, width, height) {
+	constructor(game, x, y, width, height, color= 0xFFFFFFF, lineColor = 0x000000) {
 		super(game, 0, 0);
-		this.beginFill(0xFF3300);
-		this.lineStyle(2, 0x0000FF, 1);
+		this.beginFill(color);
+		this.lineStyle(2, lineColor, 1);
     	this.drawRect(x, y, width, height);
 		this.checked = false;
 	}
--- src/objects/Grid.js
+++ src/objects/Grid.js
@@ -2,11 +2,11 @@ import Cell from 'objects/Cell';
 
 class Grid extends Phaser.Group {
 
-	constructor(game, nbRow, nbColumn, cellSize, xOrigin = 0, yOrigin = 0) {
+	constructor(game, nbRow, nbColumn, cellSize, xOrigin = 0, yOrigin = 0, cellColor = 0xFF3300, gridColor = 0x000000) {
 		super(game)
 		for(let y = 0; y < nbColumn; ++y) {
 			for(let x = 0; x < nbRow; ++x) {
-				this.add( new Cell(game, x * cellSize + xOrigin, y * cellSize + yOrigin, cellSize, cellSize) );
+				this.add( new Cell(game, x * cellSize + xOrigin, y * cellSize + yOrigin, cellSize, cellSize, cellColor, gridColor) );
 			}
 		}
 	}
