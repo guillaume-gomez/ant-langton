218c2d30f7500cda083e98c26b8a46dd8d374859
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-23 21:57:53 +0200

    add animations

--- src/objects/Ant.js
+++ src/objects/Ant.js
@@ -9,8 +9,8 @@ class Ant extends Phaser.Sprite{
 		this.antRotation = 90;
     this.animations.add('down', [0, 1, 2], 10, true);
     this.animations.add('left', [12, 13, 14], 10, true);
-    this.animations.add('right', [24, 25, 25], 10, true);
-    this.animations.add('up', [32, 33, 34], 10, true);
+    this.animations.add('right', [24, 25, 26], 10, true);
+    this.animations.add('up', [36, 37, 38], 10, true);
 
     this.game = game;
 	}
@@ -45,21 +45,37 @@ class Ant extends Phaser.Sprite{
   goDown() {
     const position = this.y + CellWidth;
     this.game.add.tween(this).to( { y: position }, ElapsedTime, Phaser.Easing.Linear.None, true);
+    this.animations.play("down", 45, true);
+    this.animations.stop("up");
+    this.animations.stop("left");
+    this.animations.stop("right");
   }
 
   goUp() {
     const position = this.y - CellWidth;
     this.game.add.tween(this).to( { y: position }, ElapsedTime, Phaser.Easing.Linear.None, true);
+    this.animations.play("up", 45, true);
+    this.animations.stop("down");
+    this.animations.stop("left");
+    this.animations.stop("right");
   }
 
   turnLeft() {
     const position = this.x - CellWidth;
     this.game.add.tween(this).to( { x: position }, ElapsedTime, Phaser.Easing.Linear.None, true);
+    this.animations.play("left", 45, true);
+    this.animations.stop("down");
+    this.animations.stop("up");
+    this.animations.stop("right");
   }
 
   turnRight() {
     const position = this.x + CellWidth;
     this.game.add.tween(this).to( { x: position }, ElapsedTime, Phaser.Easing.Linear.None, true);
+    this.animations.play("right", 45, true);
+    this.animations.stop("down");
+    this.animations.stop("left");
+    this.animations.stop("up");
   }
 }
 
