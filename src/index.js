b2ea3cd2540f78d7d0810b0ebfa52d832d470f91
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-09 21:11:34 +0200

    use constant

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import GameState from 'states/GameState';
+import { WidthCanvas, HeighCanvas}  from './constants';
 
 class Game extends Phaser.Game {
 
 	constructor() {
-		super(800, 600, Phaser.AUTO, 'content', null);
+		super(WidthCanvas, HeighCanvas, Phaser.AUTO, 'content', null);
 		this.state.add('GameState', GameState, false);
 		this.state.start('GameState');
 	}
